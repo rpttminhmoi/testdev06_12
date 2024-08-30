b7a9b1338c630a800530652b8cd2a67537e2a970
rpttminhmoi <kudokaito12345@gmail.com>
2024-08-30 17:32:37 +0700

    file developtest

--- scripts.js
+++ scripts.js
@@ -1,22 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuItems = document.querySelectorAll('.menu-item');
-    const sections = document.querySelectorAll('.section');
+    const sections = document.querySelectorAll('.content');
 
-    // Hiển thị section đầu tiên mặc định
     sections[0].classList.add('active');
+    menuItems[0].classList.add('active');
 
     menuItems.forEach(item => {
         item.addEventListener('click', function(e) {
             e.preventDefault();
 
-            // Ẩn tất cả các section
             sections.forEach(section => {
                 section.classList.remove('active');
             });
 
-            // Hiển thị section được chọn
+            menuItems.forEach(menuItem => {
+                menuItem.classList.remove('active');
+            });
             const sectionId = this.getAttribute('data-section');
             document.getElementById(sectionId).classList.add('active');
+            this.classList.add('active');
         });
     });
 });
